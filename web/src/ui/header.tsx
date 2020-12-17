5452a1c24286c0f31d41bdabc274d912339311d2
Alexey Paramonov <alexey.paramonov@bravedevelopers.com>
2020-12-17 20:19:27 +0400

    sttle

--- web/src/ui/header.tsx
+++ web/src/ui/header.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react'
-import {Box, Container} from '@material-ui/core'
+import {Box, Container, Button} from '@material-ui/core'
 import {ButtonBase} from "./button"
 import Cookies from "js-cookie"
 import {useHistory} from "react-router-dom"
 import {rootRoutes} from "../route/routes"
+
+
 const Header = (): ReactElement =>{
   const history=useHistory()
 
@@ -12,10 +14,25 @@ const Header = (): ReactElement =>{
     Cookies.remove("userName")
     history.push(rootRoutes.login)
   }
+
+  const login =():void =>{
+    history.push(rootRoutes.login)
+  }
+
   return (
     <Box width="100vw" height="50px" bgcolor="#3f51b5" display="flex" justifyContent="center">
-      <Box fontSize="32px" color="white" fontWeight="500" > WebChat </Box>
-      <ButtonBase text="logout" onClick={logout} />
+
+      <Box width="50vw" justifyContent="flex-end" fontSize="32px" color="white" fontWeight="500" > WebChat </Box>
+
+      <Box display="flex" justifyContent="flex-end" width="50vw">
+        <Box bgcolor="white" border="1px solid white" borderRadius="10px" height="40px">
+          <Button  onClick={login} color="primary">login</Button>
+        </Box>
+
+        <Box bgcolor="white" border="1px solid white" borderRadius="10px" height="40px">
+          <Button  onClick={logout} color="primary" >logout</Button>
+        </Box>
+      </Box>
     </Box>
   )
 }
