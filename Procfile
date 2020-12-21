32369897c9efeea0b9910a19329261116332992a
Alexey Paramonov <alexey.paramonov@bravedevelopers.com>
2020-12-21 13:59:54 +0400

    feat(api)

--- Procfile
+++
@@ -1,2 +0,0 @@
-cd api
-npm run start:prod
--- api/src/main.ts
+++ api/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     subscriptionsEndpoint: `/subscriptions`,
   }));
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
